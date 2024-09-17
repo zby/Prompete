9bc1fa292cf5485974f7900667547915e628d1ab
Zbigniew Lukasiak <zzbbyy@gmail.com>
2024-09-17 20:57:26 +0200

    example typos fixed; ollama example model added

--- examples/response_format_structured_output.py
+++ examples/response_format_structured_output.py
@@ -10,15 +10,15 @@ class Address(BaseModel):
 
 class Company(BaseModel):
     name: str
-    speciality: str
+    specialty: str
     address: Address
 
 
 class CompaniesList(BaseModel):
-    companies_list: list[Company]
+    companies: list[Company]
 
     def print_companies(self):
-        pprint(self.companies_list, indent=4)
+        pprint(self.companies, indent=4)
 
 
 # pprint(get_tool_defs([print_companies]))
@@ -28,8 +28,9 @@ with open(file_path, "r") as file:
     story = file.read()
 
 # model = "claude-3-haiku-20240307"  # Currently Anthropic does not support response_format - but we emulate it by using tools
+#model="ollama_chat/llama3:instruct" 
 model = "gpt-4o-mini"
-chat = Chat(model=model)
+chat = Chat(model=model, emulate_response_format=True)
 
 prompt = f"{story}\n\nPlease print the information about companies mentioned in the text above."
 
@@ -41,17 +42,17 @@ reply_struct.print_companies()
 [
     Company(
         name="Aether Innovations",
-        speciality="Sustainable energy solutions",
+        specialty="Sustainable energy solutions",
         address=Address(street="150 Futura Plaza", city="Metropolis"),
     ),
     Company(
         name="Gastronauts",
-        speciality="Urban dining experiences",
+        specialty="Urban dining experiences",
         address=Address(street="45 Flavor Street", city="Gourmet Grove"),
     ),
     Company(
         name="SereneScape",
-        speciality="Digital wellness",
+        specialty="Digital wellness",
         address=Address(street="800 Tranquil Trail", city="Metropolis"),
     ),
 ]
